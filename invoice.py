301776f0602c91874a1c41d6a380b8008a066346
szymonwolkowski <szymon.wolkowski@gmail.com>
2024-07-28 14:23:56 +0400

    save

--- invoice.py
+++ invoice.py
@@ -3,19 +3,24 @@ st.subheader("Invoice")
 Country = "UAE"
 street = "gjklajkfgl"
 city = "Sharja"
-st.sidebar.checkbox
-
-Left,Right = st.columns(2)
-st.write(Country)
-st.write(city)
-st.write(street)
-st.write("\n")
-st.write("Bill To:")
-with Left:
-    st.text_input("Enter name:",label_visibility="collapsed",placeholder="Name:")
-    st.text_input("Enter mail:",label_visibility="collapsed",placeholder="Gmail")
-with Right:
-    RL,RR = st.columns(2)
-    with RL:
-        st.write("Invoice#")
-        st.write("Invoice Date")
+T = st.sidebar.radio("Invoice",["Invoice generator","Location Edit"])
+if T == "Invoice generator":
+    st.write(Country)
+    st.write(city)
+    st.write(street)
+    st.write("\n")
+    st.write("Bill To:")
+    Left,Right = st.columns(2)
+    with Left:
+        st.text_input("Enter name:",label_visibility="collapsed",placeholder="Name:")
+        st.text_input("Enter mail:",label_visibility="collapsed",placeholder="Gmail")
+    with Right:
+        RL,RR = st.columns(2)
+        with RL:
+            st.write("Invoice#")
+            st.write("Invoice Date")
+            st.write("Due Date")
+        with RR:
+            st.text_input("Invoice#",label_visibility = "collapsed")
+            st.date_input("Invoice date",label_visibility = "collapsed")
+            st.date_input("Due date",label_visibility = "collapsed")
---
+++ walktothepark.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.title("Walk to the park")
+st.write("""
+As I shut the door of my house it made a slammed behind me.
+ After some time of walking I saw a cat walking on the street purring as it got some food from some kids.
+A chirping bird flew over my head and the cat darted hissing to the bird.
+I caught the cat so it wont scare the cat anymore and put it down off the road.
+Now I was able to see some trees of the park a bit far away.
+I headed quickly stomping loudly on the sidewalk  scaring the singing birds.
+""")
